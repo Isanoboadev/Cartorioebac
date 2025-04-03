abcd80d6e3785ec7c89104976c1fe1f354e4b3a8
Isanoboadev <inoboa28@gmail.com>
2025-04-03 05:37:36 -0300

    Opção de sair
    dispor ao usuario a opção de sair

--- Cartorio.consulta.cpp
+++ Cartorio.consulta.cpp
@@ -132,6 +132,7 @@ int main()
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar Nomes\n");
+		printf("\t4 - Sair do Sistema\n\n");
 		printf("Opção: ");//fim do menu
 	
 		scanf("%d" , &opcao); //armazenando a escolha do usuario
@@ -152,6 +153,11 @@ int main()
 			case 3:
 			deletar();
 			break;
+			
+			case 4:
+			printf("Obrigada por utilizar o sistema!\n");
+			return 0;
+			break;	
 			
 			default:
 			printf("Essa opção não esta disponivel!\n");
